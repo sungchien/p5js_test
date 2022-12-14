4f0fa8e0044d70b5fa9744fd6c59341cb2aa02c6
webdesign-106 <37160319+webdesign-106@users.noreply.github.com>
2022-12-14 14:46:28 +0800

    12/14

--- 05_circles_if_else/sketch.js
+++ 05_circles_if_else/sketch.js
@@ -1,15 +1,29 @@
+let toggle = 0;
+
 function setup() {
     // put setup code here
     createCanvas(480, 360);
+    angleMode(DEGREES);
 }
 function draw() {
     // put drawing code here
     if (mouseIsPressed) {
-        fill(color('rgba(255, 0, 0, 0.1)'));
+        noStroke();
+        if (toggle==0) {
+            fill(color('rgba(255, 0, 0, 0.1)'));
+            ellipse(mouseX, mouseY, 30, 30);
+        }
+        else if (toggle==1) {
+            fill(color('rgba(0, 255, 0, 0.1)'));
+            square(mouseX, mouseY, 30);
+        }
+        else {
+            fill(color('rgba(0, 0, 255, 0.1)'));
+            triangle(mouseX+15*sin(180), mouseY+15*cos(180), mouseX+15*sin(60), mouseY+15*cos(60), mouseX+15*sin(300), mouseY+15*cos(300));
+        }
     }
-    else {
-        fill(255);
-    }
-    noStroke();
-    ellipse(mouseX, mouseY, 30, 30);
+}
+
+function mouseClicked() {
+    toggle = (toggle+1) % 3;
 }
